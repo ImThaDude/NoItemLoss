0af290d2546373b451e0b2070f49aebaa28f40e7
ImThaDude <jorge_quan@hotmail.com>
2016-01-14 15:58:09 -0800

    Forgot to make it an Event Handler...

--- src/noitemloss/PlayerDeathListener.java
+++ src/noitemloss/PlayerDeathListener.java
@@ -9,6 +9,7 @@ import java.util.Iterator;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.inventory.ItemStack;
@@ -21,6 +22,7 @@ This will listen to player death and cut inventory in floor half. When player di
 
 public class PlayerDeathListener implements Listener {
     
+    @EventHandler
     public void onPlayerDeath(PlayerDeathEvent event) {
 
         Player p = event.getEntity();
